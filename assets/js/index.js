f61bbd17f1aabee49000ada5b542c3fa187e2f62
Hector Garcia <garcia27h@hotmail.com>
2023-10-25 22:45:29 -0300

    Desafio parte 2 terminado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,31 @@
 var borde = document.querySelector("#imagen")
 
-function aparecer(){
-    if(borde.style.border){
-        borde.style.border = "none"
-    } else{
+function aparecer() {
+    if (borde.style.border) {
+        borde.style.border = ""
+    } else {
         borde.style.border = "solid 2px red"
     }
 }
+
+var stk1 = document.querySelector("#luffy1").value
+var stk2 = document.querySelector("#zoro1").value
+var stk3 = document.querySelector("#sanji1").value
+var texto = document.querySelector("#parrafo")
+valor1 = parseInt(stk1)
+valor2 = parseInt(stk2)
+valor3 = parseInt(stk3)
+valortotal = valor1 + valor2 + valor3
+function sumar() {
+
+    if (valortotal <= 10) {
+        texto.innerHTML = "Usted lleva " + valortotal + " Stickers";
+    } else if (valortotal > 10){
+        texto.innerHTML = "Usted lleva demasiados Stickers"
+    }else{
+        texto.innerHTML = "Debe ingresar un numero en cada campo"
+    }
+
+}
+
+
