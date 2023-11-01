8618b527751105d973da46b22a4552905efca5a4
Hector Garcia <garcia27h@hotmail.com>
2023-11-01 18:24:40 -0300

    correcciones de codigo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,7 @@
 //Parte 1
+function aparecer() {
 var borde = document.querySelector("#imagen")
 
-function aparecer() {
     if (borde.style.border) {
         borde.style.border = ""
     } else {
@@ -10,6 +10,7 @@ function aparecer() {
 }
 
 //Parte 2
+function sumar() {
 var stk1 = document.querySelector("#luffy1").value
 var stk2 = document.querySelector("#zoro1").value
 var stk3 = document.querySelector("#sanji1").value
@@ -18,7 +19,6 @@ valor1 = parseInt(stk1)
 valor2 = parseInt(stk2)
 valor3 = parseInt(stk3)
 valortotal = valor1 + valor2 + valor3
-function sumar() {
 
     if (valortotal <= 10) {
         texto.innerHTML = "Usted lleva " + valortotal + " Stickers";
@@ -31,13 +31,13 @@ function sumar() {
 
 //Parte 3
 
+function comprobar(){
 var aviso = document.querySelector("#txt")
 var numero1 = document.querySelector("#numb1").value
 var numero2 = document.querySelector("#numb2").value
 var numero3 = document.querySelector("#numb3").value
 var password = numero1 + numero2 + numero3
 
-function comprobar(){
     if (numero1 == 9 && numero2 == 1 && numero3 == 1) {
         aviso.innerHTML = "password 1 correcto"
     }else if (password == 714) {
